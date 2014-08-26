5005b35162605fbd0e857632bb55a34bfae4dbee
Luis C Vargas <lcv1984@gmail.com>
2014-08-26 19:57:02 -0400

    Salpeter IMF in test_lf.py
    Added f_salpeter. The function takes in:
    - a mass array
    - a min mass below which dN/dM = 0
    - a max mass above which dN/dM = 0
    - alpha = abs val of exponent of linear version of Salpeter, recall alpha=Gamma+1
    - calculates dM between each consec elements of mass array
    - returns dN = dN/dM * dM array

--- test_lf.py
+++ test_lf.py
@@ -16,6 +16,17 @@ def read_iso():
     f.close()
     return iso_pd
 
+#now specify a Salpeter LF, alpha is exponent in linear eqn, alpha = Gamma + 1
+
+def f_salpeter(mass_arr,mass_min,mass_max,alpha):
+    dmass_arr = np.ediff1d(mass_arr,to_end=0.0)  #to end sets last element to 0 otherwise
+       #one element too few.
+    dmass_arr[len(dmass_arr)-1] = dmass_arr[len(dmass_arr)-2] #update last element
+    dN_arr = (mass_arr**(-1.*alpha)) * dmass_arr
+    dN_arr[mass_arr < mass_min] = 0.0
+    dN_arr[mass_arr > mass_max] = 0.0
+    return dN_arr
+
 iso = read_iso()
 
 mass_min = 0.1 ; mass_max = 0.8
@@ -33,5 +44,21 @@ plt.plot(iso['mass'],iso['r'],color='b',markersize=5,marker='^')
 plt.plot(mass_new,r_new,color='r',markersize=3,marker='o')
 plt.plot(mass_new,dr_new,color='g',markersize=3,marker='o')
 plt.show()
+
+#now specify a Salpeter LF
+
+mass_arr = np.arange(0.0,2.0,0.001)
+dN_arr1 = f_salpeter(mass_arr,0.15,1.5,2.35)
+dN_arr2 = f_salpeter(mass_arr,0.15,1.5,2.00)
+dN_arr3 = f_salpeter(mass_arr,0.15,1.5,2.60)
+
+plt.semilogy(mass_arr,dN_arr1,label='alpha=2.35',color='red')
+plt.semilogy(mass_arr,dN_arr2,label='alpha=2.00',color='blue')
+plt.semilogy(mass_arr,dN_arr3,label='alpha=2.60',color='green')
+plt.xlabel('Mass [Msun]') ; plt.ylabel('dN')
+plt.title('Salpeter, Test Code')
+plt.show()
+
+
 
 
