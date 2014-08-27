666d75c4d1f1f445fcce215a509a3b1423bf1df4
Luis C Vargas <lcv1984@gmail.com>
2014-08-27 12:39:57 -0400

    Testing my LF calculation (test_lf.py)
    Description of changes:
    ======================
    - Added: read_lf module.
    - dmass_arr = > abs(dmass_arr) in dN/dM * dM calculation.
    - Compared successfully my r-band LF given a Dartmouth isochrone
      compared to the LF computed by the Dartmouth code for said
      isochrone.
    - Note: LF calculated by Dartmouth code includes non-MSTO, I
      excise those portions of the rmag array as we are not using
      non-MS stages for fitting, and checked that our MS LFs match.
    
    To do:
    =====
    - There is still a small offset in r-band (seems constant).
      This may be because I am choosing to use the rmag points in
      the isochrones as left-edges of their respective bins. To
      do more properly, need to have dMass bins between r +/- dr/2.
    - Mass - magnitude relation must be monotonic, need to
      cut off isochrone at or around MSTO (necessary?)
    
     [Originally had failed to match Dartmouth LF as the LF I
      requested from Dartmouth had x = 2.35 instead of x = -2.35]

--- test_lf.py
+++ test_lf.py
@@ -8,13 +8,21 @@ import pandas as pd
 import timeit
 
 def read_iso():
-    f = open('iso/test.iso','r')
+    f = open('iso/darth_1gy.iso','r')
     ids = ['mass','teff',';logg','g','r','i']
     iso_pd = pd.read_csv(f,comment='#',names=ids,delim_whitespace=True,
-        header=0,skiprows=8,usecols=(1,2,3,6,7,8))
+        header=None,skiprows=9,usecols=(1,2,3,6,7,8))
     iso_pd['gr'] = iso_pd['g'] - iso_pd['r']
     f.close()
     return iso_pd
+
+def read_lf():
+    f = open('iso/darth_1gy.lf','r')
+    ids = ['r','logN','logdN']
+    lf_pd = pd.read_csv(f,comment='#',names=ids,delim_whitespace=True,
+        header=None,skiprows=9,usecols=(1,2,3))
+    f.close()
+    return lf_pd
 
 #now specify a Salpeter LF, alpha is exponent in linear eqn, alpha = Gamma + 1
 
@@ -22,9 +30,13 @@ def f_salpeter(mass_arr,mass_min,mass_max,alpha):
     dmass_arr = np.ediff1d(mass_arr,to_end=0.0)  #to end sets last element to 0 otherwise
        #one element too few.
     dmass_arr[len(dmass_arr)-1] = dmass_arr[len(dmass_arr)-2] #update last element
+    dmass_arr = abs(dmass_arr)
+    plt.plot(mass_arr,dmass_arr)
+    plt.xlim(0,1.6)
+    plt.ylim(0,0.2)
+    plt.show()
     dN_arr = (mass_arr**(-1.*alpha)) * dmass_arr
-    dN_arr[mass_arr < mass_min] = 0.0
-    dN_arr[mass_arr > mass_max] = 0.0
+    dN_arr[(mass_arr < mass_min) & (mass_arr > mass_max)] = 0.0
     return dN_arr
 
 #specify a Chabrier LF, but given in dN/dM. The Chabrier IMF is given typically as dN/d(logM)
@@ -42,8 +54,16 @@ def f_chabrier(mass_arr,mass_min,mass_max,mass_crit,sigma_mass_crit):
     return dN_arr
     
 iso = read_iso()
+lf  = read_lf()
 
-mass_min = 0.1 ; mass_max = 0.8
+mass_min = 0.05 ; mass_max = 1.4
+r_min_index = np.argmin(np.abs(iso['mass']-mass_max))
+r_max_index = np.argmin(np.abs(iso['mass']-mass_min))
+r_min = iso['r'][r_min_index]
+r_max = iso['r'][r_max_index]
+
+print r_min_index,r_max_index
+print r_min,r_max
 
 #fit a spline to M - r relation
 
@@ -52,37 +72,67 @@ mass_new = np.arange(mass_min,mass_max,0.01)
 r_new = interpolate.splev(mass_new,tck,der=0)
 dr_new = interpolate.splev(mass_new,tck,der=1)
 
+#fit a spline to r - M relation
+
+in_r = iso[(iso['r'] < r_max) & (iso['r'] > r_min)]['r']
+in_mass = iso[(iso['r'] < r_max) & (iso['r'] > r_min)]['mass']
+
+print len(in_r),len(in_mass)
+
+tck = interpolate.splrep(in_r[::-1],in_mass[::-1])  #need to be sorted from small to large
+r_new_2 = np.arange(r_min,r_max,0.01)
+mass_new_2 = interpolate.splev(r_new_2,tck,der=0)
+dr_new_2 = interpolate.splev(mass_new,tck,der=1)
+
 #plot original data and spline
 
-plt.plot(iso['mass'],iso['r'],color='b',markersize=5,marker='^')
-plt.plot(mass_new,r_new,color='r',markersize=3,marker='o')
-plt.plot(mass_new,dr_new,color='g',markersize=3,marker='o')
+plt.plot(iso['mass'],iso['r'],color='b',markersize=2,marker='^')
+plt.plot(mass_new,r_new,color='r',markersize=1,marker='o')  
+plt.plot(mass_new_2,r_new_2,color='k',markersize=5,marker='+')
+#plt.plot(mass_new,dr_new,color='g',markersize=1,marker='o')
 plt.show()
 
 #now specify a Salpeter LF
 
 mass_arr = np.arange(0.0,2.0,0.001)
-dN_arr1 = f_salpeter(mass_arr,0.15,1.5,2.35)
-dN_arr2 = f_salpeter(mass_arr,0.15,1.5,2.00)
-dN_arr3 = f_salpeter(mass_arr,0.15,1.5,2.60)
 
-plt.semilogy(mass_arr,dN_arr1,label='alpha=2.35',color='red')
-plt.semilogy(mass_arr,dN_arr2,label='alpha=2.00',color='blue')
-plt.semilogy(mass_arr,dN_arr3,label='alpha=2.60',color='green')
-plt.xlabel('Mass [Msun]') ; plt.ylabel('dN')
-plt.title('Salpeter, Test Code')
-plt.show()
+#dN_arr1 = f_salpeter(mass_arr,0.1,1.5,2.35)
+#dN_arr2 = f_salpeter(mass_arr,0.1,1.5,2.00)
+#dN_arr3 = f_salpeter(mass_arr,0.1,1.5,2.60)
 
-dN_arr1 = f_chabrier(mass_arr,0.1,0.8,0.2,0.2)
-dN_arr2 = f_chabrier(mass_arr,0.1,0.8,0.2,0.4)
-dN_arr3 = f_chabrier(mass_arr,0.1,0.8,0.6,0.2)
+#plt.semilogy(mass_arr,dN_arr1,label='alpha=2.35',color='red')
+#plt.semilogy(mass_arr,dN_arr2,label='alpha=2.00',color='blue')
+#plt.semilogy(mass_arr,dN_arr3,label='alpha=2.60',color='green')
+#plt.xlabel('Mass [Msun]') ; plt.ylabel('dN')
+#plt.title('Salpeter, Test Code')
+#plt.show()
 
-plt.semilogy(mass_arr,dN_arr1,label='mc=.2,sig=.2',color='red')
-plt.semilogy(mass_arr,dN_arr2,label='mc=.2,sig=.4',color='blue')
-plt.semilogy(mass_arr,dN_arr3,label='mc=.6,sig=.2',color='green')
+#now specify a Chabrier LF
+
+dN_arr1 = f_chabrier(mass_arr,0.1,1.5,0.4,0.2)
+dN_arr2 = f_chabrier(mass_arr,0.1,1.5,0.4,0.4)
+dN_arr3 = f_chabrier(mass_arr,0.1,1.5,0.8,0.2)
+
+plt.semilogy(mass_arr,dN_arr1,label='mc=.4,sig=.2',color='red',lw=3)
+plt.semilogy(mass_arr,dN_arr2,label='mc=.4,sig=.4',color='blue')
+plt.semilogy(mass_arr,dN_arr3,label='mc=.8,sig=.2',color='green')
 plt.xlabel('Mass [Msun]') ; plt.ylabel('dN')
 plt.title('Chabrier, Test Code')
 plt.show()
 
+#Now take isochorne mass-R relation, and map onto the r indices in the LF
+mass_lf = interpolate.splev(lf['r'],tck,der=0)
+
+dN_sal_luis = np.log10(f_salpeter(mass_lf,0.1,1.2,2.35))
+dN_sal_darth = 10.**lf['logdN']
+
+y = 10.**(dN_sal_luis[np.isinf(dN_sal_luis) == False])
+x = lf['r'][np.isinf(dN_sal_luis) == False]
+
+plt.plot(lf['r'],dN_sal_darth,color='r',marker='o')
+plt.plot(x,8000*y,color='b',marker='o',markersize=2)
+plt.xlim(r_min,r_max)
+plt.ylim(0,5000)
+plt.show()
 
 
