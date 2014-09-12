158ada19d31ded78720523eb3603442e6dd0938a
Luis C Vargas <lcv1984@gmail.com>
2014-09-11 23:19:29 -0400

    main.py: test run after mods to all other programs
    - can now take either real or simulated data
    
    - will plot CMD of the real/sim data.
    
    - calculate likelihood for different alpha values.
    
    - known issues:
    
      * likelihood code still has likelihood spikes towards the edges of
        the mass array.
    
      * likelihood rises with increasing power law alpha in both sim and
        real data w.o. bound, because there is no normalization to the
        number of stars per bin.
    
      * salpeter and chabrier modules may still have a very small
        magnitude shift in their luminosity functions relative to
        the dartmouth LF in the literature, because of how dN is
        calculated not at the center point of bins in dM, but at
        the left edge [have not checked this issue in a while]

--- main.py
+++ main.py
@@ -8,78 +8,92 @@ from scipy.interpolate import interp1d
 import pandas as pd
 import timeit
 from mywrangle import *
-
-#now specify a Salpeter LF, alpha is exponent in linear eqn, alpha = Gamma + 1
-
-def f_salpeter(mass_arr,mass_min,mass_max,alpha):
-    dmass_arr = np.ediff1d(mass_arr,to_end=0.0)  #to end sets last element to 0 otherwise
-       #one element too few.
-    dmass_arr[len(dmass_arr)-1] = dmass_arr[len(dmass_arr)-2] #update last element
-    dmass_arr = abs(dmass_arr)
-    dN_arr = (mass_arr**(-1.*alpha)) * dmass_arr
-    dN_arr[(mass_arr < mass_min) & (mass_arr > mass_max)] = 0.0
-    return dN_arr
-
-#specify a Chabrier LF, but given in dN/dM. The Chabrier IMF is given typically as dN/d(logM)
-#dN/dM = (1/ln10)*(1/M)*dN/dlogM, and this is calculated within the function. Finally, return
-#dM, as for f_salpeter .
-#Careful: denominator in first term has ln10 = np.log(10), but exponential is log10 M, so np.log10(m)
-def f_chabrier(mass_arr,mass_min,mass_max,mass_crit,sigma_mass_crit):
-    dmass_arr = np.ediff1d(mass_arr,to_end=0.0)  
-    dmass_arr[len(dmass_arr)-1] = dmass_arr[len(dmass_arr)-2] 
-    dmass_arr = abs(dmass_arr)
-    dN_arr = ((1./(np.log(10.)*mass_arr)) * (1./(np.sqrt(2.*np.pi)*sigma_mass_crit)) * 
-        np.exp(-1. * (np.log10(mass_arr)-np.log10(mass_crit))**2 / (2. * sigma_mass_crit**2)) * 
-        dmass_arr)
-    dN_arr[(mass_arr < mass_min) & (mass_arr > mass_max)] = 0.0
-    return dN_arr
-    
-def likelihood_matrix(cmd_point,iso_point,error_cov):
-    """Perform calculations as ndarrays and not as matrices;  have
-    checked that the behavior and cpu usage is the same"""
-    diff = cmd_point - iso_point
-    arg = -0.5*(np.dot(diff,np.dot(np.linalg.inv(error_cov),diff)))
-    #print diff,arg
-    return arg
-
-def likelihood(sigma_r,sigma_gr,cov_gr_r,delta_gr_arr,delta_r_arr):
-    #arrays must be ndarr, not python lists
-    det_sigma_matrix = sigma_r*sigma_r*sigma_gr*sigma_gr - cov_gr_r*cov_gr_r
-    det_sigma_matrix_inv = 1.0 / det_sigma_matrix
-    P = 1.0/(2.0*np.pi*np.sqrt(det_sigma_matrix))
-    exp_arg = np.exp(-0.5*(det_sigma_matrix_inv)*
-          (sigma_r**2*delta_gr_arr**2 - 
-           2.0*cov_gr_r*delta_gr_arr*delta_r_arr + 
-           sigma_gr**2*delta_r_arr**2))
-    #print P*exp_arg
-    return P*exp_arg
+from myanalysis import *
 
 #Set env variables for latex-style plotting
-if len(sys.argv) != 2: sys.exit()
 rc('text', usetex=True)
 rc('font', family='serif')
 
-system = 'wfpc2'
+sim = 1
 
-dsph_select = str(sys.argv[1])
+if sim == 1:
 
-#Read-in MW dwarf spheroidal data, e.g., Mv, distance modulus, velocity dispersion
-#The data comes from a data table I try to maintain updated with high quality data 
-#for each quantity.
-dsphs = read_dsph_data()
+    titlestring = 'Simulated'
 
-dmod0  = dsphs.loc[dsph_select,'dmod0']  
-ra_dwarf  = dsphs.loc[dsph_select,'ra'] 
-dec_dwarf  = dsphs.loc[dsph_select,'dec']  
-rhalf_dwarf  = dsphs.loc[dsph_select,'r_h']  
-print 'The distance modulus is {0:4f}.'.format(dmod0)
-print 'The central ra is {0:4f} deg.'.format(ra_dwarf)
-print 'The central decl is {0:4f} deg.'.format(dec_dwarf)
-print 'The half-light radius is {0:4f} arcmin.'.format(rhalf_dwarf)
+    system = 'acs'
+    sysmag1   = 'F606W'
+    sysmag2   = 'F814W'
 
-#Read in photometry database and extract relevant quantities
-phot = read_phot(dsph_select,dataset=system,cuts=True)
-phot_raw = read_phot(dsph_select,dataset=system,cuts=False)
+    isoage = 14.0
+    isofeh = -2.5
+    isoafe =  0.4
+    dmod0  = 20.63  #dmod to Hercules
+    nstars = 4500
+    mass_min = 0.20
+    mass_max = 0.80
+
+    #Use Herc data to model error as a function of magnitude
+    phot = read_phot('Herc',system,sysmag1,sysmag2,cuts=True)
+    magsort1 = np.argsort(phot['F606W'])
+    magsort2 = np.argsort(phot['F814W'])
+    p1 = np.polyfit(phot['F606W'][magsort1],phot['F606Werr'][magsort1],4,cov=False)
+    p2 = np.polyfit(phot['F814W'][magsort2],phot['F814Werr'][magsort2],4,cov=False)
+    magarr1 = np.arange(22.,30.,.01)  
+    magarr2 = np.copy(magarr1)
+    magerrarr1 = np.polyval(p1,magarr1)
+    magerrarr1[magarr1 <= phot['F606W'].min()] = phot['F606Werr'].min()
+    magerrarr2 = np.polyval(p2,magarr2)
+    magerrarr2[magarr2 <= phot['F814W'].min()] = phot['F814Werr'].min()
+    phot_true = np.copy(phot)
+    del phot
+
+    #Create simulated data. simulate_cmd module called from myanalysis.py
+    #For test version, see test_simulate_cmd.py
+    phot = simulate_cmd(nstars,isoage,isofeh,isoafe,dmod0,magarr1,magerrarr1,magarr2,magerrarr2,
+    system,sysmag1,sysmag2,imftype='salpeter',alpha=2.35,mass_min=mass_min,mass_max=mass_max)
+
+    phot_raw = np.copy(phot)
+
+elif sim == 0:
+
+    if len(sys.argv) != 2: sys.exit()
+
+    system = 'acs'
+    sysmag1   = 'F606W'
+    sysmag2   = 'F814W'
+
+    isoage = 14.0
+    isofeh = -2.5
+    isoafe =  0.4
+    dmod0  = 20.63  #dmod to Hercules
+    #nstars = 10000  #only for simulated data
+    mass_min = 0.05
+    mass_max = 0.75  #mass max must be below MSTO - make plot to check for this?
+
+    dsph_select = str(sys.argv[1])
+    titlestring = dsph_select
+
+    #Read-in MW dwarf spheroidal data, e.g., Mv, distance modulus, velocity dispersion
+    #The data comes from a data table I try to maintain updated with high quality data 
+    #for each quantity.
+    dsphs = read_dsph_data()
+
+    dmod0  = dsphs.loc[dsph_select,'dmod0']  
+    ra_dwarf  = dsphs.loc[dsph_select,'ra'] 
+    dec_dwarf  = dsphs.loc[dsph_select,'dec']  
+    rhalf_dwarf  = dsphs.loc[dsph_select,'r_h']  
+    print '=============================='
+    print 'The distance modulus is {0:4f}.'.format(dmod0)
+    print 'The central ra is {0:4f} deg.'.format(ra_dwarf)
+    print 'The central decl is {0:4f} deg.'.format(dec_dwarf)
+    print 'The half-light radius is {0:4f} arcmin.'.format(rhalf_dwarf)
+    print '=============================='
+
+    #Read in photometry database and extract relevant quantities
+    phot = read_phot('Herc',system,sysmag1,sysmag2,cuts=True)
+    phot_raw = read_phot('Herc',system,sysmag1,sysmag2,cuts=False)
+    #phot = read_phot(dsph_select,dataset=system,cuts=True)
+    #phot_raw = read_phot(dsph_select,dataset=system,cuts=False)
 
 #Find representative errors for bins in magnitude
 magmin = 18. ; dmag = 0.5
@@ -87,45 +101,33 @@ magbin = np.arange(18.,30.,.5) + .5  #excludes endpoint 25.
 magerrmean = []
 
 for mag in magbin:
-    magerrmean.append(phot[(phot['mag'] > mag - 0.5) & (phot['mag'] < mag + 0.5)].magerr.mean())
+    magerrmean.append(phot[(phot[sysmag2] > mag - 0.5) & (phot[sysmag2] < mag + 0.5)][sysmag2+'err'].mean())
  
 magerrmean = np.array(magerrmean)
 #Print a few elements of the matrix
 for i in range(0,2):
-    print  'rerr = {0:3f}'.format(phot.loc[i,'magerr'])
-    print  'cov = {0:3f}'.format(phot.loc[i,'cov'] - phot.loc[i,'magerr']**2)
-    print  'grerr = {0:3f}'.format(phot.loc[i,'colerr'])
-
-
-#Loop over data points and isochrone points 
-#dmod = 19.11  #McConnachie2012
-#EBV  = 0.017  #McConnachie2012
-
-#del phot
-#phot = photnew
-
-# & (phot.r0 >= rmin_box) & (phot.r0 <= rmax_box)]
-    
-mass_min = 0.05
-mass_max = 0.75  #mass max must be below MSTO - make plot to check for this?
-isoage = 14.0
-isofeh = -2.5
-isoafe = 0.4
+    print  'MAG_ERR = {0:3f}'.format(phot[i][sysmag2+'err'])
+    print  'COVAR = {0:3f}'.format(phot[i]['covar'] - phot[i][sysmag2+'err']**2)
+    print  'COLOR_ERR = {0:3f}'.format(phot[i]['colorerr'])
 
 #Now import isochrone of given age, [Fe/H], [a/Fe], making desired mass cuts for fitting
 iso = read_iso_darth(isoage,isofeh,isoafe,system,mass_min=mass_min,mass_max=mass_max)
 iso0 = read_iso_darth(isoage,isofeh,isoafe,system)
 
-isomass = iso['mass'] ; isomass0 = iso0['mass']
+isomass = iso['mass'] 
+isocol = iso[sysmag1] - iso[sysmag2] 
+isomag = iso[sysmag2] + dmod0
+
+isomass0 = iso0['mass'] 
+isocol0 = iso0[sysmag1] - iso0[sysmag2] 
+isomag0 = iso0[sysmag2] + dmod0
+
 if system == 'wfpc2':
-    isocol = iso['f606w'] - iso['f814w'] ; isocol0 = iso0['f606w'] - iso0['f814w']
-    isomag = iso['f814w'] + dmod0 ; isomag0 = iso0['f814w'] + dmod0
-    col_name = r'$m_{606w} - m_{814w}$' ; mag_name = r'$m_{814w}$'
+    col_name = r'F606W - F814W' ; mag_name = r'F814W'
 elif system == 'sdss':
-    isocol = iso['sdss_g'] - iso['sdss_r'] ; isocol0 = iso0['sdss_g'] - iso0['sdss_r']
-    isomag = iso['sdss_r'] + dmod0 ; isomag0 = iso0['sdss_r'] + dmod0
-    col_name = r'$(g - r)_0$'
-    mag_name = r'$r_0$'
+    col_name = r'$(g - r)_0$' ; mag_name = r'$r_0$'
+if system == 'acs':
+    col_name = r'F606W - F814W' ; mag_name = r'F814W'
 else:
     pass
 
@@ -138,14 +140,19 @@ if 1:
    plt.plot(isocol,isomag,lw=3,ls='--')
    plt.ylabel(mag_name)
    plt.xlabel(col_name)
+   if system == 'acs': plt.axis([-1.25,0.75,10.+dmod0,0+dmod0])
    if system == 'wfpc2': plt.axis([-1.25,0.75,10.+dmod0,0+dmod0])
    if system == 'sdss': plt.axis([-1.25,0.75,6.+dmod0,-2+dmod0])
+   if system == 'acs': plt.errorbar(-0.9+0.0*magerrmean,magbin,xerr=magerrmean,yerr=None,fmt=None,ecolor='magenta',elinewidth=2.0)
    if system == 'wfpc2': plt.errorbar(-0.9+0.0*magerrmean,magbin,xerr=magerrmean,yerr=None,fmt=None,ecolor='magenta',elinewidth=2.0)
    if system == 'sdss': plt.errorbar(-0.2+0.0*magerrmean,magbin,xerr=magerrmean,yerr=None,fmt=None,ecolor='magenta',elinewidth=2.0)
-   plt.scatter(phot_raw['col'],phot_raw['mag'],color='k',marker='.',s=1)
-   plt.scatter(phot['col'],phot['mag'],color='r',marker='o',s=2)
+   plt.scatter(phot_raw['color'],phot_raw[sysmag2],color='k',marker='.',s=1)
+   plt.scatter(phot['color'],phot[sysmag2],color='r',marker='o',s=2)
    #plt.savefig(os.getenv('HOME')+'/Desktop/fitting_data.png',bbox_inches='tight')
+   plt.title(titlestring)  #loc=1, urh; loc=2: ul; 3: ll; 4: lr; 5: r
    plt.show()
+
+#raise SystemExit
 
 """Here, I can play with interpolating isochrone to a regular grid in say rmag
 isomag   = x
@@ -167,19 +174,18 @@ tic = timeit.default_timer()
 for ialpha,alpha in enumerate(alpha_arr):
     logL_i = 0.0
     #for i in range(1000):
-    for i in range(len(phot['col'])):
-        #if i % 1000 == 0: print i
-        delta_color = phot['col'][i] - isocol
-        delta_mag   = phot['mag'][i]  - isomag
-        error_cov = np.array([[phot['colerr'][i],0.0],[0.0,phot['magerr'][i]]])
-        a  = likelihood(phot['colerr'][i],phot['magerr'][i],phot['cov'][i],delta_color,delta_mag)
+    for i in range(len(phot['color'])):
+        delta_color = phot['color'][i] - isocol
+        delta_mag   = phot[sysmag2][i]  - isomag
+        error_cov = np.array([[phot['colorerr'][i],0.0],[0.0,phot[sysmag2+'err'][i]]])
+        a  = likelihood(phot['colorerr'][i],phot[sysmag2+'err'][i],phot['covar'][i],delta_color,delta_mag)
         dN = f_salpeter(isomass,mass_min,mass_max,alpha)
         L_tmp = np.sum(a*dN)
-        if L_tmp < 1e-100: logL_tmp = -1000.
-        if L_tmp >= 1e-100: logL_tmp = np.log(L_tmp)
+        if L_tmp < 1e-200: logL_tmp = -5000.
+        if L_tmp >= 1e-200: logL_tmp = np.log(L_tmp)
         logL_i += logL_tmp
         print i,logL_i
-        if 0:
+        if 1:
             plt.subplot(2,2,1)
             plt.ylabel(r'$\rho$exp(...)')
             plt.plot(isomass,a*dN,'bo',ms=3,ls='-')
@@ -190,11 +196,11 @@ for ialpha,alpha in enumerate(alpha_arr):
             plt.ylabel(r'exp(...)')
             plt.plot(isomass,a,'bo',ms=3,ls='-')
             plt.subplot(2,2,4)
-            plt.ylabel(r'$r_0$')
-            plt.xlabel(r'$(g-r)_0$')
-            plt.axis([-0.5,1.0,7.+dmod0,-2+dmod0])
-            plt.scatter(phot_raw['gr0'],phot_raw['r0'],marker='.',s=1)
-            plt.scatter(phot['gr0'][i],phot['r0'][i],marker='o',color='red',s=8)
+            plt.ylabel(r'$'+sysmag2+'$')
+            plt.xlabel(r'$'+sysmag1+' - '+sysmag2+'$')
+            plt.axis([-1.0,1.0,11.+dmod0,0+dmod0])
+            plt.scatter(phot_raw['color'],phot_raw[sysmag2],marker='.',s=1)
+            plt.scatter(phot['color'][i],phot[sysmag2][i],marker='o',color='red',s=8)
             plt.show()
     logL_arr[ialpha] = logL_i   
 
