49bf42e60dd91f4cf3e224886fbb6b4b02f79152
Luis C Vargas <lcv1984@gmail.com>
2014-09-15 14:08:43 -0400

    new: test_required.py, see prev commit

---
+++ test_required.py
@@ -0,0 +1,7 @@
+from myanalysis import * 
+from mywrangle import * 
+import numpy as np
+
+print estimate_required_n(4000,14.0,-2.5,0.4,'acs','F814W',20.63,24.3,28.8,imftype='salpeter',alpha=2.35)
+
+
